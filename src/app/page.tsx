8e1e402a0c8fe2c7a09c90b61c31762f32d92cec
nitverse <nmakwana496@gmail.com>
2024-06-24 17:18:10 +0530

    added Basic Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BackGround from "@/components/BackGround";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Main Page
+    <main className="flex min-h-screen ">
+      <BackGround />
+     
     </main>
   );
 }
